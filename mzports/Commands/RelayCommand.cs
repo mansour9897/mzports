6746d54d36efaad798de9f186575896a9b717f2d
Mansour Marvasti <mansour.marvasti@outlook.com>
2022-03-05 14:46:22 +0330

    make relay command

---
+++ mzports/Commands/RelayCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace mzports.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private readonly Action _action;
+
+        public RelayCommand(Action action) => _action = action;
+        public bool CanExecute(object? parameter)
+        {
+            return parameter == null ? true : (bool)parameter;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _action();
+        }
+    }
+}
