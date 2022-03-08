f1ae9faaef918fc91674a8c62cd8bee492b2a5d2
Mansour Marvasti <mansour.marvasti@outlook.com>
2022-03-08 05:11:35 +0330

    make TemperatureControllerModule

---
+++ mzports.Services/TCM/TemperatureControllerModule.cs
@@ -0,0 +1,24 @@
+﻿using Communications;
+using mzports.Services.TCM.DeviceCommands;
+
+namespace mzports.Services.TCM
+{
+    public class TemperatureControllerModule : IDevices
+    {
+        private readonly ICommunication _com;
+        private readonly ITcmSelfCheckCommand _tcmSelfCheckCommand;
+
+        public TemperatureControllerModule(ICommunication com)
+        {
+            _com = com;
+            _tcmSelfCheckCommand = new TcmSelfCheckCommand(_com);
+
+        }
+
+        public bool SelfCheck()
+        {
+            _tcmSelfCheckCommand.Execute();
+            return _tcmSelfCheckCommand.ExecuteConfirmed;
+        }
+    }
+}
