77db96b2d8a7d40c578c2591fc50ac1acac136ed
Mansour Marvasti <mansour.marvasti@outlook.com>
2022-03-08 11:54:59 +0330

    fill select port combo box in tcm view toolbar

--- mzports/ViewModels/TcmViewModel.cs
+++ mzports/ViewModels/TcmViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using mzports.Services.TCM;
+using System.Collections.ObjectModel;
+using System.IO.Ports;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -7,10 +9,18 @@ namespace mzports.ViewModels
     public class TcmViewModel : ViewModelBase
     {
         private readonly TemperatureControllerModule _tcm;
+
+        public ObservableCollection<string> PortNames { get; private set; }
         public TcmViewModel(TemperatureControllerModule tcm)
         {
             _tcm = tcm;
             _ = Task.Run(() => GetDevieNameAsync());
+            PortNames = new ObservableCollection<string>();
+            var ports = SerialPort.GetPortNames();
+            foreach (var item in ports)
+            {
+                PortNames.Add(item);
+            }
         }
 
         public ICommand RangeApplyCommand => new Commands.RelayCommand(() => RangeApply());
