6cbca61af07b2f7002fff5ba30ffec177b488c6e
Mansour Marvasti <mansour.marvasti@outlook.com>
2022-03-14 16:51:07 +0330

    create SetTransmitterMinCommand

---
+++ mzports.Services/TCM/DeviceCommands/SetTransmitterMinCommand.cs
@@ -0,0 +1,16 @@
+﻿using Communications;
+
+namespace mzports.Services.TCM.DeviceCommands
+{
+    class SetTransmitterMinCommand : DeviceCommand
+    {
+        private const string opcode = "?11";
+        private const string confirm = "!11";
+        public SetTransmitterMinCommand(double setVal, ICommunication com) : base(GetCode(setVal), GetConfirmCode(setVal), com)
+        {
+        }
+
+        private static string GetCode(double val) => string.Format("{0}\t{1:0.00}\r", opcode, val);
+        private static string GetConfirmCode(double val) => string.Format("{0}\t{1:0.00}\r", confirm, val);
+    }
+}
