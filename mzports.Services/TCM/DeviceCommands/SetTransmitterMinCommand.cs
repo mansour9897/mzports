c6ef7d964fe865a065c35e42ac10ad436c4819df
Mansour Marvasti <mansour.marvasti@outlook.com>
2022-03-15 11:02:51 +0330

    add setTransmitterMaxcommand class

--- mzports.Services/TCM/DeviceCommands/SetTransmitterMinCommand.cs
+++ mzports.Services/TCM/DeviceCommands/SetTransmitterMinCommand.cs
@@ -2,15 +2,13 @@
 
 namespace mzports.Services.TCM.DeviceCommands
 {
-    class SetTransmitterMinCommand : DeviceCommand
+    class SetTransmitterMinCommand : SetValueCommand
     {
         private const string opcode = "?11";
         private const string confirm = "!11";
-        public SetTransmitterMinCommand(double setVal, ICommunication com) : base(GetCode(setVal), GetConfirmCode(setVal), com)
+        public SetTransmitterMinCommand(double setVal, ICommunication com) : base(opcode,confirm,setVal, com)
         {
         }
 
-        private static string GetCode(double val) => string.Format("{0}\t{1:0.00}\r", opcode, val);
-        private static string GetConfirmCode(double val) => string.Format("{0}\t{1:0.00}\r", confirm, val);
     }
 }
