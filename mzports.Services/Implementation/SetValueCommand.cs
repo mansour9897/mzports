4e42b17c82e088928c762bbefa2fc8a965014e2a
Mansour Marvasti <mansour.marvasti@outlook.com>
2022-03-15 10:50:20 +0330

    create SetValueCommand clas

---
+++ mzports.Services/Implementation/SetValueCommand.cs
@@ -0,0 +1,16 @@
+﻿using Communications;
+
+namespace mzports.Services.Implementation
+{
+    class SetValueCommand : DeviceCommand
+    {
+        public SetValueCommand(string _opcode,string _confirmCode,double val,ICommunication com):
+            base(GetCode(val,_opcode),GetConfirmCode(val,_confirmCode),com)
+        {
+
+        }
+
+        private static string GetCode(double val,string opcode) => string.Format("{0}\t{1:0.00}\r", opcode, val);
+        private static string GetConfirmCode(double val,string confirmCode) => string.Format("{0}\t{1:0.00}\r", confirmCode, val);
+    }
+}
